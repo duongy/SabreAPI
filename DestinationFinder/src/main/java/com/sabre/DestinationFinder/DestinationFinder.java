0f381d12c3dd0e4634acbe2b224bb321b4e85fbd
Kenneth Adam Miller <kennethadammiller@gmail.com>
2014-08-02 08:56:45 -0500

    blah

--- DestinationFinder/src/main/java/com/sabre/DestinationFinder/DestinationFinder.java
+++ DestinationFinder/src/main/java/com/sabre/DestinationFinder/DestinationFinder.java
@@ -1,7 +1,81 @@
 package com.sabre.DestinationFinder;
 
+import java.util.concurrent.ExecutionException;
+
 import com.sabre.SabreBase.DSCommHandler;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class DestinationFinder {
+	// TODO Auto-generated method stub
+	//
+	// Request authentication
+	//
+	private final String clientId = "V1:8wwal2rsmapuiak7:DEVCENTER:EXT";
+	private final String clientSecret = "3M6EVsuo";
+	private final String target = "http://api.test.sabre.com";
+	private final String endpoint = "/v1/shop/flights/fares";
+
+	// base64 encode clientId and clientSecret
+	public String request() {
+
+		String encodedClientId = new String(Base64.encodeBase64(
+				clientId.getBytes()));
+		String encodedClientSecret = new String(Base64.encodeBase64(
+				clientSecret.getBytes()));
+
+		// Concatenate encoded client and secret strings, separated with colon
+		String encodedClientIdSecret = encodedClientId + ":"
+				+ encodedClientSecret;
+
+		// Convert the encoded concatenated string to a single base64 encoded
+		// string.
+		encodedClientIdSecret = new String(Base64.encodeBase64(
+				encodedClientIdSecret.getBytes()));
+
+		DSCommHandler dsC = new DSCommHandler();
+		String token = dsC.getAuthToken("https://api.test.sabre.com",
+				encodedClientIdSecret);
+		String response = dsC.sendRequest(
+				target + endpoint, token);
+		// Display the response String
+		return response;
+
+		// dsC.sendRequest("https://api.test.sabre.com/v1/shop/flights/fares?origin=LAS&lengthofstay=5",
+
+	}
 	
+	public String asyncRequest() {
+
+		String encodedClientId = new String(Base64.encodeBase64(
+				clientId.getBytes()));
+		String encodedClientSecret = new String(Base64.encodeBase64(
+				clientSecret.getBytes()));
+
+		// Concatenate encoded client and secret strings, separated with colon
+		String encodedClientIdSecret = encodedClientId + ":"
+				+ encodedClientSecret;
+
+		// Convert the encoded concatenated string to a single base64 encoded
+		// string.
+		encodedClientIdSecret = new String(Base64.encodeBase64(
+				encodedClientIdSecret.getBytes()));
+
+		DSCommHandler dsC = new DSCommHandler();
+		try {
+			return dsC.asyncRequest(
+					target + endpoint, target, encodedClientIdSecret);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "error";
+		// Display the response String
+
+		// dsC.sendRequest("https://api.test.sabre.com/v1/shop/flights/fares?origin=LAS&lengthofstay=5",
+
+	}
 }
--- SabreBase/src/main/java/com/sabre/SabreBase/DSCommHandler.java
+++ SabreBase/src/main/java/com/sabre/SabreBase/DSCommHandler.java
@@ -8,93 +8,159 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-
+import java.util.concurrent.ExecutionException;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
 public class DSCommHandler {
+	private URL urlConn;
+	private URLConnection conn;
 
-    public String getAuthToken(String apiEndPoint, String encodedCliAndSecret){
-      //receives : apiEndPoint (https://api.test.sabre.com)
-      //sabre//encodedCliAndSecret : base64Encode(  base64Encode(V1:[user]:[group]:[domain]) + ":" + base64Encode([secret]) )
-      String strRet = null;
+	public String getAuthToken(String apiEndPoint, String encodedCliAndSecret) {
+		// receives : apiEndPoint (https://api.test.sabre.com)
+		// sabre//encodedCliAndSecret : base64Encode(
+		// base64Encode(V1:[user]:[group]:[domain]) + ":" +
+		// base64Encode([secret]) )
+		String strRet = null;
 
-      try {
+		try {
 
-        URL urlConn = new URL(apiEndPoint + "/v1/auth/token");
-        URLConnection conn=urlConn.openConnection();
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        conn.setUseCaches(false);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Authorization", "Basic " + encodedCliAndSecret);
-        conn.setRequestProperty("Accept", "application/json");
-        DataOutputStream dataOut = new DataOutputStream(conn.getOutputStream());
-        dataOut.writeBytes("grant_type=client_credentials");
-        dataOut.flush();
-        dataOut.close();
+			urlConn = new URL(apiEndPoint + "/v1/auth/token");
+			conn = urlConn.openConnection();
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setUseCaches(false);
+			conn.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+			conn.setRequestProperty("Authorization", "Basic "
+					+ encodedCliAndSecret);
+			conn.setRequestProperty("Accept", "application/json");
+			DataOutputStream dataOut = new DataOutputStream(
+					conn.getOutputStream());
+			dataOut.writeBytes("grant_type=client_credentials");
+			dataOut.flush();
+			dataOut.close();
 
-        //get response
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String strChunk="";
-        StringBuilder sb = new StringBuilder();
-        while(null != ((strChunk=rd.readLine())))
-          sb.append(strChunk);
+			// get response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String strChunk = "";
+			StringBuilder sb = new StringBuilder();
+			while (null != ((strChunk = rd.readLine())))
+				sb.append(strChunk);
 
-        //parse the token
-        JSONObject respParser = new JSONObject(sb.toString());
-        strRet = respParser.getString("access_token");
-    } catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (JSONException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+			// parse the token
+			JSONObject respParser = new JSONObject(sb.toString());
+			strRet = respParser.getString("access_token");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    return strRet;
-  }
+		return strRet;
+	}
 
-  @SuppressWarnings("deprecation")
-  public String sendRequest(String payLoad,String authToken){
-    URLConnection conn=null;
-    String strRet=null;
-    try {
-      URL urlConn = new URL(payLoad);
+	@SuppressWarnings("deprecation")
+	public String sendRequest(String payLoad, String authToken) {
+		URLConnection conn = null;
+		String strRet = null;
+		try {
+			URL urlConn = new URL(payLoad);
 
-      conn = null;
-      conn = urlConn.openConnection();
+			conn = null;
+			conn = urlConn.openConnection();
 
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setUseCaches(false);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setUseCaches(false);
 
-      conn.setRequestProperty("Authorization", "Bearer " + authToken);
-      conn.setRequestProperty("Accept", "application/json");
+			conn.setRequestProperty("Authorization", "Bearer " + authToken);
+			conn.setRequestProperty("Accept", "application/json");
 
+			DataInputStream dataIn = new DataInputStream(conn.getInputStream());
+			String strChunk = "";
+			StringBuilder sb = new StringBuilder("");
+			while (null != ((strChunk = dataIn.readLine())))
+				sb.append(strChunk);
 
-      DataInputStream dataIn = new DataInputStream(conn.getInputStream());
-      String strChunk="";
-      StringBuilder sb = new StringBuilder("");
-      while(null != ((strChunk = dataIn.readLine())))
-        sb.append(strChunk);
+			strRet = sb.toString();
 
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("IOException: " + conn.getHeaderField(0));
+		}
+		return strRet;
+	}
 
-      strRet = sb.toString();
+	public String asyncRequest(final String payLoad, String target, String encodedClientIdSecret) throws InterruptedException, ExecutionException {
+		AsyncRequest t1 = new AsyncRequest(payLoad, target, encodedClientIdSecret);
+		return (String) t1.execute().get();
+	}
 
+	private class AsyncRequest extends AsyncTask {
+		public String strRet = null;
+		private String payLoad = null;
+		private String authToken = null;
+		private String target=null;
+		private String encodedClientIdSecret=null;
 
-    } catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      System.out.println("IOException: "+conn.getHeaderField(0));
-    }
-    return strRet;
-  }
+		public AsyncRequest(String payLoad, String target, String encodedClientIdSecret) {
+			this.payLoad = payLoad;
+			this.target=target;
+			this.encodedClientIdSecret=encodedClientIdSecret;
+		}
+
+		public Object doInBackground(Object... params) {
+			this.authToken = 
+					getAuthToken(target,
+							encodedClientIdSecret);
+			URLConnection conn = null;
+			try {
+				URL urlConn = new URL(payLoad);
+
+				conn = null;
+				conn = urlConn.openConnection();
+
+				conn.setDoInput(true);
+				conn.setDoOutput(true);
+				conn.setUseCaches(false);
+
+				conn.setRequestProperty("Authorization", "Bearer " + authToken);
+				conn.setRequestProperty("Accept", "application/json");
+				DataInputStream dataIn = new DataInputStream(
+						conn.getInputStream());
+				String strChunk = "";
+				StringBuilder sb = new StringBuilder("");
+				while (null != ((strChunk = dataIn.readLine())))
+					sb.append(strChunk);
+
+				strRet = sb.toString();
+
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("IOException: " + conn.getHeaderField(0));
+			}
+			if (conn!=null)
+				Log.i("Connection status: " , "");
+			return strRet;
+		}
+	}
 }
